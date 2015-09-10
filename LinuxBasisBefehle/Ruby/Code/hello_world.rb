7d948304ad26d57f229e9a144daa358128868be4
sylver21v <viktor.kreis@gmx.de>
2015-09-10 19:08:33 +0200

    ’LinuxDocs’

---
+++ LinuxBasisBefehle/Ruby/Code/hello_world.rb
@@ -0,0 +1,8 @@
+puts "Die \"Woche\" besteht aus:\n\tMontag.\n\tDienstag."
+
+var1 = "Hello Ruby"
+var2 = "heute ist der 10.09.2015 + "
+var3 = 1
+puts var1 + " " + var2 + var3.to_s
+
+puts "#{var1} #{Time.new} #{var3}"
---
+++ LinuxBasisBefehle/hello_world.rb
@@ -0,0 +1,2 @@
+var = "Hello Ruby"
+puts var
